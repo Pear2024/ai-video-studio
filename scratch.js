15b98a09a1dca0891f30b4cda00fa0ae52ed87b6
Pear2024 <155395970+Pear2024@users.noreply.github.com>
2026-05-09 14:58:46 -0700

    Remove leaked API key script

--- scratch.js
+++
@@ -1,9 +0,0 @@
-const apiKey = "AIzaSyBcl6WV98ofEvsAxgK5h3_tkHpDLbJgigs";
-
-async function listModels() {
-  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`);
-  const data = await res.json();
-  console.log(JSON.stringify(data, null, 2));
-}
-
-listModels();
